b037fb25c830daef7a6ae71aac02ff06f3ab5d90
Pavel Zvonnikov <pavelzvonnikov@yandex.ru>
2019-10-07 18:24:51 +0300

    init todoapp

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app" class="container">
+    <ToDoList />
+  </div>  
+</template>
+
+<script>
+import ToDoList from "./components/ToDoList.vue"
+export default {
+  name: 'app',
+  components: {
+    ToDoList
+  },
+  data() {
+    return {
+      
+    }
+  },
+
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  #app {
+    font-family: 'Avenir', Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 24px;
+  }
+</style>
